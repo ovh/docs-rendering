564a72b006ddddcd1a3104c9fa6c94446bbec74b
Jeremy <52998075+jeremylatorre@users.noreply.github.com>
2022-06-08 08:05:32 +0000

    update header menu.es-es.html

---
+++ themes/ovh/static/sites/default/files/js/js_4Z0jeQ4Ez80cuVZ2ICq5uuS-66EnmajtOrmAvlUCKBA.js
@@ -0,0 +1,40 @@
+(function (Drupal) {
+  Drupal.t('Help center', {}, { context: 'ovh' });
+  Drupal.t('Hello. How can we help you?', {}, { context: 'ovh' });
+  Drupal.t('Search', {}, { context: 'ovh' });
+  Drupal.t('Submit the query', {}, { context: 'ovh' });
+  Drupal.t('Filters', {}, { context: 'ovh' });
+  Drupal.t('Languages', {}, { context: 'ovh' });
+  Drupal.t('Articles type', {}, { context: 'ovh' });
+  Drupal.t('Search results for', {}, { context: 'ovh' });
+  Drupal.t('No results found', {}, { context: 'ovh' });
+  Drupal.t('@firstHit - @lastHit to @nbHits results', {}, { context: 'ovh' });
+  Drupal.t('Previous page', {}, { context: 'ovh' });
+  Drupal.t('Current page', {}, { context: 'ovh' });
+  Drupal.t('Next page', {}, { context: 'ovh' });
+  Drupal.t('Go to page @page', {}, { context: 'ovh' });
+  Drupal.t('Find out more', {}, { context: 'ovh' });
+  Drupal.t('<strong>@query</strong>', {}, { context: 'ovh' });
+  Drupal.formatPlural(
+    1,
+    '<strong>1 result</strong> for your query "<strong>@query</strong>"',
+    '<strong>@count results</strong> for your query "<strong>@query</strong>"',
+    {},
+    { context: 'ovh' },
+  );
+  Drupal.t('Search', {}, { context: 'ovh' });
+  Drupal.t('See all results', {}, { context: 'ovh' });
+  Drupal.t('Solutions', {}, { context: 'ovh' });
+  Drupal.t('Products', {}, { context: 'ovh' });
+  Drupal.t('Program', {}, { context: 'ovh' });
+  Drupal.t('Guide', {}, { context: 'ovh' });
+  Drupal.t('Relevance', {}, { context: 'ovh' });
+  Drupal.t('Ascending price', {}, { context: 'ovh' });
+  Drupal.t('Descending price', {}, { context: 'ovh' });
+  Drupal.t('New', {}, { context: 'ovh' });
+  Drupal.t('Your last search: ', {}, { context: 'ovh' });
+  Drupal.t('Previous', {}, { context: 'ovh' });
+  Drupal.t('Next', {}, { context: 'ovh' });
+  Drupal.t('Search products, docs …', {}, { context: 'ovh' });
+})(Drupal);
+;
